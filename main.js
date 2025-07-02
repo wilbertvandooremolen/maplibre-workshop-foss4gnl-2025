8914aae14686906e7db7ed3dbec6d04bb57f22ee
wilbertvandooremolen <wilbert.vandooremolen@nieuwlandgeo.nl>
2025-07-02 09:55:18 +0000

    nieuwe stijl

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";
 
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
-    style: 'https://demotiles.maplibre.org/style.json', // style URL
+    //style: 'https://demotiles.maplibre.org/style.json', // style URL
+    style: './assets/style.json',
     center: [0, 0], // starting position [lng, lat]
     zoom: 1 // starting zoom
 });
